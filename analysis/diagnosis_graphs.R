3d93faa9bcf8de3d6dd8f6e88051275bda2f8afb
Allison Li <allli@cs.washington.edu>
2021-09-17 22:30:17 -0700

    finished general diagnosis graphs

---
+++ analysis/diagnosis_graphs.R
@@ -0,0 +1,35 @@
+#Import packages
+library(tidyverse)
+library(lintr)
+library(styler)
+
+#Import data
+patient_data <- read.csv("C:\\Users\\allis\\Desktop\\SightLife-Patient-Data-Project\\data\\FINAL LVPEI Bhubaneswar Tx Data Cleaned (2000s).csv")
+
+#Extract Diagnosis
+diagnosis_count <- table(patient_data$GEN_INDICATION_OF_PK) 
+diagnosis_count <- data.frame(diagnosis_count)
+colnames(diagnosis_count) <- c("condition", "patients")
+
+#delete first row full of 0s
+diagnosis_count <- diagnosis_count[-1,]
+
+#create additional column with percentages
+total <- sum(diagnosis_count$patients)
+diagnosis_count$prop <- with(diagnosis_count, patients/total)
+
+#create donut graph
+#Compute the cumulative percentages (top of each rectangle)
+diagnosis_count$ymax = cumsum(diagnosis_count$prop)
+
+#Compute the bottom of each rectangle
+diagnosis_count$ymin = c(0, head(diagnosis_count$ymax, n=-1))
+
+#visualize plot
+diagnosis_donut <- ggplot(diagnosis_count, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=condition)) +
+  geom_rect() +
+  coord_polar(theta="y") + 
+  scale_fill_brewer(palette=3) +
+  scale_color_brewer(palette=3) +
+  xlim(c(2, 4)) +
+  theme_void()
